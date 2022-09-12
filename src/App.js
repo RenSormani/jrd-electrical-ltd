1f3b9ccfa313a8b78098e31a043e88dbdc4766aa
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-09-12 14:05:39 +0100

    changes to App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss'
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar'
-import Home from './components/Pages/Home';
+import Home from './components/Pages/Home/Home';
 import Contact from './components/Pages/Contact/Contact';
 import About from './components/Pages/About/About';
 import TrustMark from './components/Pages/TrustMark/TrustMark';
@@ -15,12 +15,11 @@ import ScrollToTop from './container/ScrollToTop/ScrollToTop';
 
 const App = () => {
   return (
-    <BrowserRouter history={history} basename= {...env.PUBLIC_URL}>,
       <Router>
         <ScrollToTop />
           <Navbar />
             <Routes>
-              <Route exact path='/' element={<Home />} />
+              <Route exact path='/jrd-electrical-ltd/' element={<Home />} />
               <Route path='/services' element={<Service />} />
               <Route path='/trustmark' element={<TrustMark />} />
               <Route path='/about' element={<About />} />
@@ -30,7 +29,6 @@ const App = () => {
             </Routes>
           <Footer />
       </Router>
-      </BrowserRouter>
   )
 }
 
