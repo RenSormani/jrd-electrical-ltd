83fb4aee5ae5167eb1228ed7918643d4fb026de8
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-09-13 10:27:27 +0100

    formatted

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import '../../App.scss'
-import { Button } from '../Button/Button'
-import './HeroSection.scss'
- 
+import React from "react";
+import "../../App.scss";
+import { Button } from "../Button/Button";
+import "./HeroSection.scss";
+
 const HeroSection = () => {
   return (
-    <div className='hero-container'>
+    <div className="hero-container">
       <h1>JRD ELECTRICAL</h1>
-      <div className='hero-text'>
-      <p>Shockingly good electricians since 1991</p>
+      <div className="hero-text">
+        <p>Shockingly good electricians since 1991</p>
       </div>
-      <div className='hero-btns'>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
         >
           GET A QUOTE
         </Button>
       </div>
     </div>
   );
-}
+};
 
-export default HeroSection
+export default HeroSection;
